41450ddb188ecc92143799adebcfc82e96d67d0e
Bgabor-prog <79143542+Bgabor-prog@users.noreply.github.com>
2021-05-09 16:20:51 +0200

    Add files via upload

--- QVTQO8_0309/QVTQO8gyak1.c
+++ QVTQO8_0309/QVTQO8gyak1.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main()
+int main(void)
 {
     int		status;
+    if (!system("dir"))
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%i\n", i);
+        }
+    }
 
-	if ( (status = system("hibas")) < 0)
-		perror("system() error");
+    system("repa");
 
-	if (WIFEXITED(status))
+    if (WIFEXITED(status))
 		printf("Hibas befejezodes, visszaadott ertek = %d\n", WEXITSTATUS(status));
 
 	exit(0);
+
     return 0;
 }
